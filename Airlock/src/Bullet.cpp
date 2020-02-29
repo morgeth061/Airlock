5b524d691fc3a1d2113f89eb752a358cd63d2e38
BlaqueBower <59838860+BlaqueBower@users.noreply.github.com>
2020-02-28 22:09:56 -0500

    Added Comments
    Includes Labels for:
    Bullet.cpp
    Enemy.cpp
    Minerals.cpp

--- Airlock/src/Bullet.cpp
+++ Airlock/src/Bullet.cpp
@@ -1,3 +1,8 @@
+/*
+BULLET class
+- deals with player bullet functionality
+*/
+
 #include <iostream>
 #include<vector>
 #include<algorithm>
@@ -6,6 +11,7 @@
 #include "Game.h"
 #define WIDTH 1024
 
+// Bullet Ctor
 Bullet::Bullet(SDL_Rect s, SDL_Rect d, int spd)
 {
 	setType(GameObjectType::BULLET);
@@ -13,16 +19,18 @@ Bullet::Bullet(SDL_Rect s, SDL_Rect d, int spd)
 	setIsHit(false);
 	setBulletDmg(25);
 	m_dst = { d.x , d.y , 4,4 };
+	//Makes sure Bullets shoots the way the player is looking
 	if (Game::Instance()->m_pTarget->getFlip() == SDL_FLIP_HORIZONTAL)
 		headRight = true;
 	else
 		headRight = false;
 }
 
+// Bullet De-Ctor
 Bullet::~Bullet()
 {
 }
-
+// Bullet Update
 void Bullet::update()
 {
 	if (headRight)
@@ -34,13 +42,13 @@ void Bullet::update()
 		active = false;
 	}
 }
-
+// Bullet Render
 void Bullet::render()
 {
 	SDL_SetRenderDrawColor(Game::Instance()->getRenderer(), 255, 0, 0, 255);
 	SDL_RenderFillRect(Game::Instance()->getRenderer(), &m_dst);	   
 }
-
+// Clean on Exit
 void Bullet::clean()
 {
 }
@@ -49,11 +57,11 @@ void Bullet::draw()
 {
 }
 
+//Bullet Damage 
 void Bullet::setBulletDmg(int damage)
 {
 	m_bulletDmg = damage;
 }
-
 int Bullet::getBulletDmg()
 {
 	return m_bulletDmg;
